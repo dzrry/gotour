8e6515b000adae90754a8397c84ee64708072f2a
dzrry <dzrrymihael@gmail.com>
2020-04-13 23:40:48 +0700

    task 5

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"github.com/dzrry/gotour/task2"
 	"github.com/dzrry/gotour/task3"
+	"github.com/dzrry/gotour/task5"
 	"golang.org/x/tour/pic"
 	"golang.org/x/tour/wc"
 )
@@ -12,4 +14,12 @@ func main() {
 	pic.Show(task2.Pic)
 	// task 3 maps
 	wc.Test(task3.WordCounts)
+	//task 5 stringers
+	hosts := map[string]task5.IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
 }
